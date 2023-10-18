0c30a81fe5d910c0025855d0bfeab7df799d3854
Arthur (@Refzlund) <arthur@refzlund.com>
2023-10-18 04:31:05 +0000

    Reformatted the generic example

--- packages/@api-kit/src/_examples/generic-endpoint.ts
+++ packages/@api-kit/src/_examples/generic-endpoint.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { endpoint } from '$/endpoint'
-import { EndpointRequestInput as RQ, RequestOptions } from '$/endpoint/callback'
-import { KitEvent, KitRequestInput } from '$/endpoint/event'
+import { RequestOptions } from '$/endpoint/callback'
+import { KitEvent } from '$/endpoint/event'
 import { parseJSON } from '$/endpoint/functions'
-import { InferGenericEndpoint as I, InferBody, InferOptions } from '$/endpoint/generic'
+import { InferBody, InferOptions } from '$/endpoint/generic'
 import { OK, BadRequest } from '$/response'
-import { APIKit, GenericAPIKit } from '$/apikit'
+import { GenericAPIKit } from '$/apikit'
 import { ToJSON } from '$/types/json'
 
 /*
@@ -14,9 +14,7 @@ import { ToJSON } from '$/types/json'
 	easily readable, unless T has been defined.
 */
 
-
-// * Notice it extends ToJSON
-interface Put extends ToJSON<{
+interface Put {
 	body: {
 		name: string
 		date?: Date
@@ -24,18 +22,18 @@ interface Put extends ToJSON<{
 	query: {
 		seasonId: number
 	}
-}> {}
+} 
 
-// * Notice it passes the event to endpoints first parameter
-const PUT = <T extends Put>(e: KitEvent<T>) => endpoint(e)(
+// * Important:  Notice it `extends ToJSON<Put>` and not just `extends Put`
+// * Important:  Notice it passes the event to endpoints first parameter
+const PUT = <T extends ToJSON<Put>>(e: KitEvent<T>) => endpoint(e)(
 	parseJSON,
 	(event) => {
 		return OK({
 			b: event.locals.json,
 			q: {} as T['query']
 		})
-	},
-	() => BadRequest({ error: 'End of pipe' })
+	}
 )
 
 // * Notice it uses `GenericAPIKit` instead of `APIKit
@@ -46,7 +44,7 @@ const x = {} as {
 	>(body: B, options: O) => GenericAPIKit<typeof PUT<{ body: B } & O>>
 }
 
-const [response] = await x.PUT({ name: 'Bob', date: new Date().toJSON() }, { query: { seasonId: 123 }, cache: 'default' }).$.OK(res => {
+const [response] = await x.PUT({ name: 'Bob', date: new Date().toJSON() }, { query: { seasonId: 123 }, cache: 'default' }).$.success(res => {
 	return res.json()
 })
 
