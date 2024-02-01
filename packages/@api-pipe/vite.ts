87afe4809564d8873d68094f1318ba1061c31770
Arthur @Refzlund <arthur@refzlund.com>
2024-02-01 06:55:25 +0000

    remove the unneccesary `import.meta.dir` from  `path.resolve`

--- packages/@api-pipe/vite.ts
+++ packages/@api-pipe/vite.ts
@@ -2,5 +2,5 @@ import path from 'path'
 import build from 'vite-configs/build'
 
 build({
-	tsconfigPath: path.resolve(import.meta.dir, './tsconfig.json')
+	tsconfigPath: path.resolve('./tsconfig.json')
 }, 'index.ts')
--- packages/@api-responses/vite.ts
+++ packages/@api-responses/vite.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import build from 'vite-configs/build'
 
-build({ tsconfigPath: path.resolve(import.meta.dir, './tsconfig.json')},
+build({ tsconfigPath: path.resolve('./tsconfig.json')},
 	'index.ts', 'types.ts'
 )
